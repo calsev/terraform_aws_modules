4490d5d06e003776d5a42e6d87e47fadcc87c201
Caleb Severn <calsev@gmail.com>
2022-11-08 11:35:35 -0800

    Add module for ecs amis

---
+++ ecs_ami_map/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  ami_map = {
+    for tag, _ in local.ami_tag_to_parameter_path : tag => jsondecode(data.aws_ssm_parameter.ecs_optimized_ami[tag].value)
+  }
+  ami_tag_to_parameter_path = {
+    cpu_amd = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended",
+    cpu_arm = "/aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended",
+    gpu_amd = "/aws/service/ecs/optimized-ami/amazon-linux-2/gpu/recommended",
+  }
+}
