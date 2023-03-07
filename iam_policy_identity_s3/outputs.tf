3eed035016dce75f3772434c237eea5df6d84a9e
Caleb Severn <calsev@gmail.com>
2023-03-07 09:26:11 -0800

    Regularize s3 policy output

--- iam_policy_identity_s3/outputs.tf
+++ iam_policy_identity_s3/outputs.tf
@@ -1,7 +1,6 @@
-output "iam_policy_arn" {
-  value = module.this_policy.iam_policy_arn
-}
-
-output "iam_policy_doc" {
-  value = jsondecode(data.aws_iam_policy_document.this_policy_doc.json)
+output "data" {
+  value = {
+    iam_policy_arn = module.this_policy.iam_policy_arn
+    iam_policy_doc = jsondecode(data.aws_iam_policy_document.this_policy_doc.json)
+  }
 }
