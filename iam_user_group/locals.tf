80e48b90d0aee0d5ba572bcb79bd5311f368de49
Caleb Severn <calsev@gmail.com>
2023-04-18 17:19:56 -0700

    Remove json from user output

--- iam_user_group/locals.tf
+++ iam_user_group/locals.tf
@@ -59,9 +59,14 @@ locals {
       })
     }
     user = {
-      for k, v in local.user_map : k => merge(v, {
-        arn = aws_iam_user.this_user[k].arn
-      })
+      for k, v in local.user_map : k => merge(
+        {
+          for k_user, v_user in v : k_user => v_user if !contains(["policy_inline_json_map"], k_user)
+        },
+        {
+          arn = aws_iam_user.this_user[k].arn
+        },
+      )
     }
   }
 }
