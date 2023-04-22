b378d50eef4052bc059973cfd3fc24f7b38ba568
Caleb Severn <calsev@gmail.com>
2023-04-22 11:28:48 -0700

    Format message for batch alert

--- batch_job/alert.tf
+++ batch_job/alert.tf
@@ -2,8 +2,10 @@ module "alert_trigger" {
   source = "../event_trigger"
   event_map = {
     for k, v in local.alert_map : v.k_alert => {
-      event_pattern_json = jsonencode(v.alert_event_pattern_doc)
-      target_arn         = var.monitor_data.alert.topic_map[v.alert_level].topic_arn
+      event_pattern_json                = jsonencode(v.alert_event_pattern_doc)
+      input_transformer_path_map        = v.alert_target_path_map
+      input_transformer_template_string = v.alert_target_template
+      target_arn                        = var.monitor_data.alert.topic_map[v.alert_level].topic_arn
     }
   }
   event_target_service_default = "sns"
