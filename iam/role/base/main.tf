e492f9a1d5862645f6938e1e2b37c9df83b950c3
Caleb Severn <calsev@gmail.com>
2024-11-04 13:34:03 -0800

    Use resources for role exclusive policies

--- iam/role/base/main.tf
+++ iam/role/base/main.tf
@@ -18,25 +18,31 @@ resource "aws_iam_policy" "this_created_policy" {
 resource "aws_iam_role" "this_iam_role" {
   assume_role_policy   = jsonencode(local.l2_map.assume_role_doc)
   max_session_duration = local.l1_map.max_session_duration_m == null ? null : local.l1_map.max_session_duration_m * 60
-  dynamic "inline_policy" {
-    for_each = local.l1_map.role_policy_inline_doc_map == null ? {} : local.l1_map.role_policy_inline_doc_map
-    content {
-      name   = inline_policy.key
-      policy = jsonencode(inline_policy.value)
-    }
-  }
-  dynamic "inline_policy" {
-    # This ensures an empty policy
-    for_each = local.l1_map.role_policy_inline_doc_map == null ? { this = {} } : length(local.l1_map.role_policy_inline_doc_map) == 0 ? { this = {} } : {}
-    content {}
-  }
   lifecycle {
     create_before_destroy = true
   }
-  managed_policy_arns = [for _, arn in local.l4_map.role_policy_all_attached_arn_map : arn]
-  name                = local.l1_map.name_effective
-  path                = local.l1_map.role_path
-  tags                = local.l1_map.tags
+  name = local.l1_map.name_effective
+  path = local.l1_map.role_path
+  tags = local.l1_map.tags
+}
+
+resource "aws_iam_role_policy" "inline_policies" {
+  for_each = local.l1_map.role_policy_inline_doc_map
+  name     = each.key
+  policy   = jsonencode(each.value)
+  role     = aws_iam_role.this_iam_role.id
+}
+
+resource "aws_iam_role_policies_exclusive" "inline_policies" {
+  role_name = aws_iam_role.this_iam_role.name
+  policy_names = length(local.l1_map.role_policy_inline_doc_map) == 0 ? [] : [
+    for k, v in local.l1_map.role_policy_inline_doc_map : k
+  ]
+}
+
+resource "aws_iam_role_policy_attachments_exclusive" "managed_policies" {
+  role_name   = aws_iam_role.this_iam_role.name
+  policy_arns = [for _, arn in local.l4_map.role_policy_all_attached_arn_map : arn]
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
