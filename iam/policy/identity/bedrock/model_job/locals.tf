8acdc2599091bfcd0c908d0cc79305e008affde6
Caleb Severn <calsev@gmail.com>
2025-01-08 13:45:30 -0800

    Cross-region IAM for Bedrock models

--- iam/policy/identity/bedrock/model_job/locals.tf
+++ iam/policy/identity/bedrock/model_job/locals.tf
@@ -1,6 +1,6 @@
 locals {
   arn_list_application_inference_profile = [
-    for name in sort(distinct(var.name_list_application_inference_profile)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:${var.std_map.aws_region_name}:${var.std_map.aws_account_id}:application-inference-profile/${name}"
+    for name in sort(distinct(var.name_list_application_inference_profile)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:*:${var.std_map.aws_account_id}:application-inference-profile/${name}"
   ]
   arn_list_job_evaluation = [
     for name in sort(distinct(var.name_list_job_evaluation)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:${var.std_map.aws_region_name}:${var.std_map.aws_account_id}:evaluation-job/${name}"
@@ -15,13 +15,13 @@ locals {
     for name in sort(distinct(var.name_list_job_model_invocation)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:${var.std_map.aws_region_name}:${var.std_map.aws_account_id}:model-invocation-job/${name}"
   ]
   arn_list_inference_profile = [
-    for name in sort(distinct(var.name_list_inference_profile)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:${var.std_map.aws_region_name}:${var.std_map.aws_account_id}:inference-profile/${name}"
+    for name in sort(distinct(var.name_list_inference_profile)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:*:${var.std_map.aws_account_id}:inference-profile/${name}"
   ]
   arn_list_model_custom = [
     for name in sort(distinct(var.name_list_model_custom)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:${var.std_map.aws_region_name}:${var.std_map.aws_account_id}:custom-model/${name}"
   ]
   arn_list_model_foundation = [
-    for name in sort(distinct(var.name_list_model_foundation)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:${var.std_map.aws_region_name}::foundation-model/${name}"
+    for name in sort(distinct(var.name_list_model_foundation)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:*::foundation-model/${name}"
   ]
   arn_list_model_provisioned = [
     for name in sort(distinct(var.name_list_model_provisioned)) : startswith(name, "arn:") ? name : "arn:${var.std_map.iam_partition}:bedrock:${var.std_map.aws_region_name}:${var.std_map.aws_account_id}:provisioned-model/${name}"
